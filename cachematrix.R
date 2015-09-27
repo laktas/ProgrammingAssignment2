98baaec30a182f7f649db5dd68a8e578d9c59126
laktas <aktas@ou.edu>
2015-09-27 17:24:32 -0500

    Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverting matrix is a computationally intensive operation.
+## Following functions make the process faster.
 
-## Write a short comment describing this function
+## makeCacheMatrix sets and gets the matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+      m<-NULL
+      set <- function (y) {
+            x<<-y
+            m<<-NULL
+      }
+      get <- function () x
+      setinv<- function(inv) m<<-inv
+      getinv<- function() m
+      list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks if matrix is in the cache, if yes, uses the
+## existing inverse value.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      m<-x$getinv()
+      if(!is.null(m)){
+            message("getting cached data")
+            return (m)
+            
+      }
+      data <- x$get()
+      m<-solve(data,...)
+      x$setinv(m)
+      m
 }
